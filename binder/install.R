e674944721308f9b49bbf7800bf226638145d6a0
Noam Ross <ross@ecohealthalliance.org>
2019-05-26 17:47:32 -0400

    Reconfigure binder directory to use Dockerfile

--- binder/install.R
+++
@@ -1,9 +0,0 @@
-install.packages('remotes')
-
-##### Install specific package versions from CRAN #####
-
-remotes::install_version("mgcv", "1.8-24")
-remotes::install_version("ggplot2", "3.0.0")
-remotes::install_version("gamair", "1.0-0", type="source")
-remotes::install_version("sp", "1.3-1")
-remotes::install_version("MASS", "7.3-50")
