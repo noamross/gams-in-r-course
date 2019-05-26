a0dc5a5dd9f30b336be5ed0c82493a9890664af8
Noam Ross <ross@ecohealthalliance.org>
2019-05-26 16:27:03 -0400

    Add introduction text

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,17 @@ export default ({ data }) => {
     return (
         <Layout isHome>
             <Logo className={classes.logo} aria-label={siteMetadata.title} />
+            <section>
+                <h1 className={classes.subtitle}>A Free, Interactive Course using <strong>mgcv</strong></h1>
+                <div className={classes.introduction}>
+                <p>
+                    Hello! Welcome to Generalized Additive Models in R.  This short course will teach you how to use these flexible, powerful tools to model data and solve data science problems. GAMs offer offer a middle ground between simple linear models and complex machine-learning techniques, allowing you to model and understand complex systems.
+                </p>
+                <p>
+                    To take this course, you need basic R programming skills and experience with linear regression. Now let's get started!
+                </p>
+                </div>
+            </section>
             {chapters.map(({ slug, title, description }) => (
                 <section key={slug} className={classes.chapter}>
                     <h2 className={classes.chapterTitle}>
