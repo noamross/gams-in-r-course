7a2402225d30f92ff2add01deac6004533480fd7
Noam Ross <ross@ecohealthalliance.org>
2019-05-26 17:07:27 -0400

    Add dependencies

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,9 @@
-install.packages("tidyverse")
-install.packages("rjson")
+ibrary(remotes)
+
+##### Install specific package versions from CRAN #####
+
+install_version("mgcv", "1.8-24")
+install_version("ggplot2", "3.0.0")
+install_version("gamair", "1.0-0", type="source")
+install_version("sp", "1.3-1")
+install_version("MASS", "7.3-50")
