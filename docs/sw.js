96af4e8dad300a8b41f1e233361912f87c606603
Noam Ross <ross@ecohealthalliance.org>
2019-05-26 18:57:49 -0400

    fix paths

--- docs/sw.js
+++ docs/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-610ce5ccfc466f88f91a.js"
+    "url": "webpack-runtime-02253309cd5d2c2732d1.js"
   },
   {
     "url": "styles.74706255fa78800aa371.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dab8d850e5659eeae13a822688a3a752"
+    "revision": "6312d5de573b11fcff561706ab6090e3"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
@@ -67,7 +67,7 @@ const navigationRoute = new workbox.routing.NavigationRoute(({ event }) => {
   return idbKeyval.get(WHITELIST_KEY).then((customWhitelist = []) => {
     // Respond with the offline shell if we match the custom whitelist
     if (customWhitelist.includes(pathname)) {
-      const offlineShell = `/noamross/offline-plugin-app-shell-fallback/index.html`
+      const offlineShell = `/gams-in-r-course/offline-plugin-app-shell-fallback/index.html`
       const cacheName = workbox.core.cacheNames.precache
 
       return caches.match(offlineShell, { cacheName }).then(cachedResponse => {
@@ -139,7 +139,7 @@ const messageApi = {
 
     pathnames = pathnames.map(({ pathname, includesPrefix }) => {
       if (!includesPrefix) {
-        return `/noamross${pathname}`
+        return `/gams-in-r-course${pathname}`
       } else {
         return pathname
       }
