659d96387532906069834ae3682c4f99800086fc
Noam Ross <ross@ecohealthalliance.org>
2019-05-26 17:27:24 -0400

    Configue gatsby for gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const autoprefixer = require('autoprefixer')
 
 module.exports = {
     siteMetadata: meta,
+    pathPrefix: "/reponame",
     plugins: [
         {
             resolve: `gatsby-plugin-sass`,
