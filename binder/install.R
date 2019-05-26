f9517b40eb2c80034591e05ec9f932d774d9646e
Noam Ross <ross@ecohealthalliance.org>
2019-05-26 17:26:05 -0400

    Install remotes first

--- binder/install.R
+++ binder/install.R
@@ -1,9 +1,9 @@
-library(remotes)
+install.packages('remotes')
 
 ##### Install specific package versions from CRAN #####
 
-install_version("mgcv", "1.8-24")
-install_version("ggplot2", "3.0.0")
-install_version("gamair", "1.0-0", type="source")
-install_version("sp", "1.3-1")
-install_version("MASS", "7.3-50")
+remotes::install_version("mgcv", "1.8-24")
+remotes::install_version("ggplot2", "3.0.0")
+remotes::install_version("gamair", "1.0-0", type="source")
+remotes::install_version("sp", "1.3-1")
+remotes::install_version("MASS", "7.3-50")
